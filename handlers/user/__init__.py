cca6a4754083bf31e39aab85091e6cc5d56f608d
Extremum <vlados8118@gmail.com>
2025-12-31 00:06:06 +1000

    Order accepting

--- handlers/user/__init__.py
+++ handlers/user/__init__.py
@@ -1,5 +1,6 @@
 from .cars.add_car import register_add_car_handlers
 from .form_manage import register_forms_manage_handlers
+from .orders.orders_list import register_orders_list_handlers
 from .profile import register_profile_handlers
 from .cars.car_manage import register_car_manage_handlers
 from .orders.new_cross_city_order import register_new_cross_city_order_handlers
@@ -19,4 +20,5 @@ def register_user_handlers(dp):
     register_new_cross_city_order_handlers(dp)
     register_orders_handlers(dp)
     register_forms_manage_handlers(dp)
+    register_orders_list_handlers(dp)
 
