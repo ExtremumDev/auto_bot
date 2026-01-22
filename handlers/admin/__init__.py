9228f6f2d0295805174ed44a762b74f03bf28b80
Extremum <vlados8118@gmail.com>
2026-01-23 00:22:48 +1000

    Orders deleting, orders manage for admin

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,5 +1,6 @@
 from handlers.admin.mailing import register_mailing_handlers
 from handlers.admin.main import register_main_admin_handlers
+from handlers.admin.orders_manage import register_admin_orders_manage_handlers
 from handlers.admin.rules import register_edit_rules_handlers
 from handlers.admin.users_manage import register_users_manage_handers
 
@@ -9,3 +10,4 @@ def register_admin_handlers(dp):
     register_edit_rules_handlers(dp)
     register_users_manage_handers(dp)
     register_mailing_handlers(dp)
+    register_admin_orders_manage_handlers(dp)
