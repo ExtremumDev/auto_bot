28420cc6bb1ab18d33854af8d44d6d2c16a01826
Extremum <vlados8118@gmail.com>
2026-02-13 20:48:31 +1000

    Searching users in user manage

--- handlers/admin/orders_manage.py
+++ handlers/admin/orders_manage.py
@@ -15,7 +15,7 @@ async def send_orders_list(c: types.CallbackQuery, db_session: AsyncSession, *ar
     await paging.get_current_page()
 
     await c.message.answer(
-        text="Выберете заказ",
+        text="Выберете заказ для администрирования",
         reply_markup=paging.get_reply_markup()
     )
     await c.answer()
