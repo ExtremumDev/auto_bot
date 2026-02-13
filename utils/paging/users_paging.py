68046ebd00a32a2a0e8cabc1f544ae4994a95f4c
Extremum <vlados8118@gmail.com>
2026-02-13 20:29:19 +1000

    Searching users in user manage

--- utils/paging/users_paging.py
+++ utils/paging/users_paging.py
@@ -36,7 +36,7 @@ class UsersPaging(Paging):
             users_list_markup.inline_keyboard.append(
                 [
                     InlineKeyboardButton(
-                        text=(u.telegram_username if u.telegram_username else str(u.telegram_id)) + " 🚫 " if u.is_blocked else " 🟢 ",
+                        text=(u.telegram_username if u.telegram_username else str(u.telegram_id)) + (" 🚫 " if u.is_blocked else " 🟢 "),
                         callback_data=f"usermanage_{u.id}"
                     )
                 ]
